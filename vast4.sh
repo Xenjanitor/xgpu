6b662c4ea02eb4536bb99f41c49de90011393c79
Xenjanitor <160481328+Xenjanitor@users.noreply.github.com>
2024-03-05 13:48:34 -0800

    Update vast4.sh

--- vast4.sh
+++ vast4.sh
@@ -31,7 +31,7 @@ echo "STEP 7 of 10: Permissions set!"
 sudo sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x81134166c117ae6C8366C36BE9e886B0F7147faE/g' config.conf > /dev/null 2>&1
 echo "STEP 8 of 10: Replaced ETH address"
 
-sudo pip install -U -r requirements.txt > /dev/null 2>&1
+sudo pip install -r requirements.txt > /dev/null 2>&1
 echo "STEP 9 of 10: Installed Python Requirements"
 
 echo "STEP 10 of 10: Starting Miner & GPU"
